384e7db0e1a8f056bc3337e612a99ca907850b0f
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2024-11-28 18:58:13 +0100

    minor changes to the config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,86 +1,86 @@
 import markdownItTaskList from 'markdown-it-task-lists';
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "ccat Documentation",
-  description: "Documentatation for the consulting contact application tool",
-  lang: "de",
+  title: 'ccat Documentation',
+  description: 'Documentatation for the consulting contact application tool',
+  lang: 'de',
   locales: {
     root: {
-      label: "German",
-      lang: "de",
-    }
+      label: 'German',
+      lang: 'de',
+    },
   },
   markdown: {
     config: (md) => {
-      md.use(markdownItTaskList)
-    }
+      md.use(markdownItTaskList);
+    },
   },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: "Einführung", link: "/" },
-      { text: "Für Anwender", link: "/users" },
-      { text: "Für Entwickler", link: "/developers" },
+      { text: 'Einführung', link: '/' },
+      { text: 'Für Anwender', link: '/users' },
+      { text: 'Für Entwickler', link: '/developers' },
     ],
     sidebar: [
-    {
-      text: "Einführung",
-      link: "/",
-    },
-    {
-      text: "Roadmap",
-      link: "/roadmap",
-    },
       {
-        text: "Examples",
+        text: 'Einführung',
+        link: '/',
+      },
+      {
+        text: 'Examples',
         items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" },
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
         ],
       },
       {
-        text: "Anwender",
-        items: [{ text: "Einführung", link: "/users" }],
+        text: 'Anwender',
+        items: [{ text: 'Einführung', link: '/users' }],
       },
       {
-        text: "Entwickler",
-        items: [{ text: "Einführung", link: "/developers" }],
+        text: 'Entwickler',
+        collapsed: false,
+        items: [
+          { text: 'Einführung', link: '/developers' },
+          { text: 'Architektur', link: '/developers/architecture' },
+        ],
       },
     ],
     search: {
-      provider: "local",
+      provider: 'local',
       options: {
         locales: {
           root: {
             translations: {
               button: {
-                buttonText: "Suche",
-                buttonAriaLabel: "Suche",
+                buttonText: 'Suche',
+                buttonAriaLabel: 'Suche',
               },
               modal: {
-                backButtonTitle: "Zurück",
-                displayDetails: "Details anzeigen",
-                noResultsText: "Keine Ergebnisse gefunden",
-                resetButtonTitle: "Zurücksetzen",
+                backButtonTitle: 'Zurück',
+                displayDetails: 'Details anzeigen',
+                noResultsText: 'Keine Ergebnisse gefunden',
+                resetButtonTitle: 'Zurücksetzen',
                 footer: {
-                  closeText: "Schließen",
-                  navigateText: "Navigieren",
-                  selectText: "Auswählen",
-                  closeKeyAriaLabel: "Schließen",
-                  navigateDownKeyAriaLabel: "Nach unten navigieren",
-                  navigateUpKeyAriaLabel: "Nach oben navigieren",
-                  selectKeyAriaLabel: "Auswählen",
-                }
-              }
-            }
-          }
-        }
-      }
+                  closeText: 'Schließen',
+                  navigateText: 'Navigieren',
+                  selectText: 'Auswählen',
+                  closeKeyAriaLabel: 'Schließen',
+                  navigateDownKeyAriaLabel: 'Nach unten navigieren',
+                  navigateUpKeyAriaLabel: 'Nach oben navigieren',
+                  selectKeyAriaLabel: 'Auswählen',
+                },
+              },
+            },
+          },
+        },
+      },
     },
     socialLinks: [
-      { icon: "github", link: "https://github.com/move-ev-sub/ccat" },
+      { icon: 'github', link: 'https://github.com/move-ev-sub/ccat' },
     ],
   },
 });
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,14 +1,14 @@
 // .vitepress/theme/index.ts
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import CardGrid from '../../components/CardGrid.vue'
-import CardLink from '../../components/CardLink.vue'
+import type { Theme } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+import CardGrid from '../../components/CardGrid.vue';
+import CardLink from '../../components/CardLink.vue';
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // register your custom global components)
-    app.component("CardLink", CardLink)
-    app.component("CardGrid", CardGrid)
-  }
-} satisfies Theme
+    app.component('CardLink', CardLink);
+    app.component('CardGrid', CardGrid);
+  },
+} satisfies Theme;
